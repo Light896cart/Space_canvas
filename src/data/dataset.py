e0fc9f07850247d24df5c69d4ab721aa0cdd4f30
Light896cart <artem.gonhcarov@gmail.com>
2025-08-31 12:55:16 +0300

    feat: Реализовал откат к альтернативному источнику изображений, если файл не найден

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,6 +1,11 @@
+import matplotlib.pyplot as plt
 import torch
+from PIL import Image
 from torch.utils.data import Dataset
 import pandas as pd
+import os
+
+from src.utils.get_image_ps1 import get_ps1_image
 
 
 class Space_dataset(Dataset):
@@ -11,21 +16,32 @@ class Space_dataset(Dataset):
         :param list_x: список столбцов для признаков (например, ['cod_class', 'cod_subclass'])
         :param list_label: список столбцов для меток (если нужны отдельно)
         """
-        df = pd.read_csv(path_csv)
-        self.list_label = df[list_x].values
-        self.list_x = df[list_x]
+        self.path_img = path_img
+        self.df = pd.read_csv(path_csv)
+        self.list_label = self.df[list_label]
+        self.list_x = self.df[list_x]
 
     def __len__(self):
         return len(self.df)
-    def __getitem__(self, item):
-        x = self.list_x.iloc[item]
-        x_tensor = torch.tensor(x, dtype=torch.long)  # или float, если нужно
-        return x_tensor
 
+    def __getitem__(self, item):
+        ide = self.df['id'].iloc[item]
+        ra, dec = self.list_x['ra'].iloc[item], self.list_x['dec'].iloc[item]
+        label = self.list_label.iloc[item].to_numpy()
+        label_tensor = torch.tensor(label, dtype=torch.long)  # или float, если нужно
+        image_path = os.path.join(self.path_img, f"{ide}.jpg")
+        print(image_path)
+        # Проверяем, есть ли файл
+        if os.path.exists(image_path):
+            image = Image.open(image_path)
+        else:
+            image = get_ps1_image(ra,dec)
+        plt.imshow(image)
+        plt.show()
+        return label_tensor
 
 path_csv = r'D:\Code\Space_canvas\data\spall_csv_chunks_encoded\spall_chunk_0001.csv'
-path_img = r'D:\Code\Space_canvas\data\image_data\img_csv\408595600150005914600060103.jpg'
-reg = Space_dataset(path_csv=path_csv,path_img=path_img,list_x=['cod_class','cod_subclass'])
+path_img = r'D:\Code\Space_canvas\data\image_data\img_csv'
+reg = Space_dataset(path_csv=path_csv,path_img=path_img,list_label=['cod_class','cod_subclass'],list_x=['ra','dec'])
 for c in reg:
     print(c)
-    break
