87d229dd327f982f6b3ae40fa3e49750347e4453
Light896cart <87309695+Light896cart@users.noreply.github.com>
2025-09-01 14:42:23 +0300

    "feat(data): добавлен DataLoader с разделением train/val и нормализацией"
    * feat: Создание DataLoader
    
    * feat(data): Создал полноценный работающие DataLoader
    
    * refactor: Переделал код, добавил разделение на тренировочные данные и валидационные
    
    * feat(data): Добавил трансформ изображения

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -10,7 +10,7 @@ from src.utils.get_image_ps1 import get_ps1_image
 
 
 class Space_dataset(Dataset):
-    def __init__(self,path_csv:str,path_img:str,list_label=None,list_x=None):
+    def __init__(self,path_csv:str,path_img:str,list_label=None,list_x=None,transform=None):
         """
         :param path_csv: путь к CSV с метаданными
         :param path_img: путь к изображениям (может не использоваться сейчас)
@@ -21,6 +21,7 @@ class Space_dataset(Dataset):
         self.df = pd.read_csv(path_csv)
         self.list_label = self.df[list_label]
         self.list_x = self.df[list_x]
+        self.transform = transform
 
     def __len__(self):
         return len(self.df)
@@ -32,7 +33,6 @@ class Space_dataset(Dataset):
         label = self.list_label.iloc[item].to_numpy()
         label_tensor = torch.tensor(label, dtype=torch.long)  # или float, если нужно
         image_path = os.path.join(self.path_img, f"{ide}.jpg")
-        print(image_path)
         # Проверяем, есть ли файл
         if os.path.exists(image_path):
             image = Image.open(image_path)
@@ -47,13 +47,14 @@ class Space_dataset(Dataset):
             # Преобразуем матрицу в изображение
             image = Image.fromarray(matrix)
             image.save(image_path)
-        print('Итерация')
-        # plt.imshow(image)
-        # plt.show()
+            # Преобразуем PIL Image → Tensor
+        if self.transform:
+            image = self.transform(image)
+
         return image,label_tensor
 
-path_csv = r'D:\Code\Space_canvas\data\spall_csv_chunks_encoded\spall_chunk_0001.csv'
-path_img = r'D:\Code\Space_canvas\data\image_data\img_csv'
-reg = Space_dataset(path_csv=path_csv,path_img=path_img,list_label=['cod_class','cod_subclass'],list_x=['ra','dec'])
-for c in reg:
-    print(c)
+# path_csv = r'D:\Code\Space_canvas\data\spall_csv_chunks_encoded\spall_chunk_0001.csv'
+# path_img = r'D:\Code\Space_canvas\data\image_data\img_csv'
+# reg = Space_dataset(path_csv=path_csv,path_img=path_img,list_label=['cod_class','cod_subclass'],list_x=['ra','dec'])
+# for c in reg:
+#     print(c)
