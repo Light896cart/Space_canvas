6a82bb1bcd7507bb6104af06443758816ec3a0b2
Light896cart <artem.gonhcarov@gmail.com>
2025-09-09 11:35:27 +0300

    refactor(data): Теперь код полноценно работает, и создает файл с идеальном распределением (доработки еще нужны)

--- scripts/one_off/distribution_data.py
+++ scripts/one_off/distribution_data.py
@@ -9,6 +9,7 @@ def mix_classes_cyclically(folder,pattern,limit):
     # Получаем файлы
     files = sorted(folder.glob(pattern))
     min_class = {}
+    first_class = 0
 
     for filename in files[:limit]:
         df = pd.read_csv(filename)
@@ -16,24 +17,37 @@ def mix_classes_cyclically(folder,pattern,limit):
         # Добавляем все пары класс → количество в словарь
         for cls, count in unique_class.items():
             min_class[cls] = min_class.get(cls, 0) + count
-        output_file = r'D:\Code\Space_canvas\data\general_csv.csv'
 
-        # Берём первый класс
-        first_class = df['cod_class'].iloc[0]
+    output_file = r'D:\Code\Space_canvas\data\general_csv.csv'
 
-        # Фильтруем DataFrame — оставляем только нужный класс
-        df_filtered = df[df['cod_class'] == first_class]
-        print(df_filtered)
-        # # Проверяем, существует ли файл
-        # file_exists = Path(output_file).is_file()
-        #
-        # # Записываем (добавляем в конец, если файл существует)
-        # df_filtered.to_csv(
-        #     output_file,
-        #     mode='a' if file_exists else 'w',  # 'a' — добавить, 'w' — перезаписать (новый файл)
-        #     index=False,
-        #     header=not file_exists  # заголовок только если файл новый
-        # )
+    changed = True
+    min_value = min(min_class.values())
+    global_min_value = min_value
+    while changed:
+        changed = False  # Сбрасываем перед началом прохода
+        print("Новый цикл со значением",first_class)
+        for filename in files[:limit]:
+            df = pd.read_csv(filename)
+            # Фильтруем DataFrame — оставляем только нужный класс
+            df_filtered = df[df['cod_class'] == first_class]
+            min_value -= len(df_filtered)
+            # Проверяем, существует ли файл
+            file_exists = Path(output_file).is_file()
+
+            # Записываем (добавляем в конец, если файл существует)
+            df_filtered[:min_value].to_csv(
+                output_file,
+                mode='a' if file_exists else 'w',  # 'a' — добавить, 'w' — перезаписать (новый файл)
+                index=False,
+                header=not file_exists  # заголовок только если файл новый
+            )
+            if min_value <= 0:
+                min_value = global_min_value
+                first_class += 1
+                changed = True
+                break
+            if len(df_filtered) == 0:
+                break
 
 
 pattern = "spall_chunk_*.csv"
